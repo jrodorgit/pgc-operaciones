370ef494c35908c7751dd85c95764aa96454df53
Jose A. Rodriguez Ortega <jrodor@gmail.com>
2018-09-03 11:50:58 +0200

    Update gen.py

--- rodorgen/core/gen.py
+++ rodorgen/core/gen.py
@@ -15,10 +15,32 @@ def generaSQLOracle(entidad):
     
     #generamos SELECT_BY_PK
     sql_select_pk = rodorgen.files.utilfile.leeLineaAjustaPatron(r'./rodorgen/resources/templates/ORACLE-DML-TEMPLATE.properties','SELECT_BY_PK')
-    sql_select_pk = sql_select_pk.replace(r'{nombre}', entity['nombre'])
-    #nombrePK = rodorgen.core.ent.getAtrPK(entity)
+    sql_select_pk = sql_select_pk.replace(r'{ENTIDAD}', entity['nombre'])
     sql_select_pk = sql_select_pk.replace(r'{ATR_PK}', rodorgen.core.ent.getAtrPK(entity))
-    #print(sql_select_pk)
+    sql_select_pk = sql_select_pk.replace(r'{ATRS}', rodorgen.core.ent.getAtrs(entity))
     fSQLDML.write(sql_select_pk)
+    
+    #generamos UPDATE_BY_PK
+    sql_update_by_pk = rodorgen.files.utilfile.leeLineaAjustaPatron(r'./rodorgen/resources/templates/ORACLE-DML-TEMPLATE.properties','UPDATE_BY_PK')
+    sql_update_by_pk = sql_update_by_pk.replace(r'{ENTIDAD}', entity['nombre'])
+    sql_update_by_pk = sql_update_by_pk.replace(r'{ATR_PK}', rodorgen.core.ent.getAtrPK(entity))
+    sql_update_by_pk = sql_update_by_pk.replace(r'{ATRS}', rodorgen.core.ent.getAtrsForUpdate(entity))
+    fSQLDML.write(sql_update_by_pk)
+    
+    # generamos DELETE_BY_PK
+    sql_delete_by_pk = rodorgen.files.utilfile.leeLineaAjustaPatron(r'./rodorgen/resources/templates/ORACLE-DML-TEMPLATE.properties','DELETE_BY_PK')
+    sql_delete_by_pk = sql_delete_by_pk.replace(r'{ENTIDAD}', entity['nombre'])
+    sql_delete_by_pk = sql_delete_by_pk.replace(r'{ATR_PK}', rodorgen.core.ent.getAtrPK(entity))
+    fSQLDML.write(sql_delete_by_pk)
+    
+    # generamos SELECT BY FK
+    for fk in rodorgen.core.ent.getAtrsFK(entity):
+        sql_select_by_fk = rodorgen.files.utilfile.leeLineaAjustaPatron(r'./rodorgen/resources/templates/ORACLE-DML-TEMPLATE.properties','SELECT_BY_FK')
+        sql_select_by_fk = sql_select_by_fk.replace(r'{ENTIDAD}', entity['nombre'])
+        sql_select_by_fk = sql_select_by_fk.replace(r'{ATRS}', rodorgen.core.ent.getAtrs(entity)) 
+        sql_select_by_fk = sql_select_by_fk.replace(r'{ATR_FK}', fk)  
+        sql_select_by_fk = sql_select_by_fk.replace(r'{ATR}', fk.upper() )
+        fSQLDML.write(sql_select_by_fk)
+    
     fSQLDML.close()
     
