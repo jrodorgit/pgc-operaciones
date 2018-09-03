f46b02297fbb356950ed89316a97e54439c831b3
Jose A. Rodriguez Ortega <jrodor@gmail.com>
2018-09-03 14:04:35 +0200

    Update main.py

--- rodorgen/main.py
+++ rodorgen/main.py
@@ -3,6 +3,10 @@
 import rodorgen.files.utilfile
 import rodorgen.core.gen
 
+TEMPLATES_DML_PATH = r'./rodorgen/resources/templates/ORACLE-DML-TEMPLATE.properties'
+TEMPLATES_DDL_PATH = r'./rodorgen/resources/templates/ORACLE-DDL-TEMPLATE.sql'
+OUT_MODEL_PATH = r'./rodorgen/resources/outModelo/'
+
 def run():
     """ lanza el proceso de generacion de ficheros fuente """
     
@@ -14,6 +18,8 @@ def run():
     entidades = [rodorgen.files.utilfile.lee(f) for f in ficheros_a_procesar]
     print('Se han leido {} entidades'.format(len(entidades)))
 
-    # generador del SQL para cada entidad.
-    [rodorgen.core.gen.generaSQLOracle(e) for e in entidades]
-
+    # generador de DDl de cada entidad
+    [rodorgen.core.gen.generaSQLDDLOracle(e) for e in entidades]
+    
+    # generador del SQL DML para cada entidad.
+    [rodorgen.core.gen.generaSQLDMLOracle(e) for e in entidades]
