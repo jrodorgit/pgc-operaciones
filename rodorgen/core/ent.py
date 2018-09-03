6208e9a7873d5ddaf3255264503965f542f8b13d
Jose A. Rodriguez Ortega <jrodor@gmail.com>
2018-09-03 11:50:04 +0200

    Update ent.py

--- rodorgen/core/ent.py
+++ rodorgen/core/ent.py
@@ -10,3 +10,27 @@ def getAtrPK(entidad):
         break;
     return atrpk
 
+def getAtrsFK(entidad):
+    """ devuelve lsitado de atributo que son FK """
+    atrfk = []
+    atrs = [a for a in entidad['atributos']]
+    for atr in atrs:
+        if atr['fk'] == "FK":
+            atrfk.append(atr['nombreatr'])
+    
+    print(atrfk)
+    return atrfk
+    
+def getAtrs(entidad):
+    """ devuelve los atributos de una entidad separados por comas """
+    atrs = [a for a in entidad['atributos']]
+    return ', '.join([''.join( x['nombreatr'] ) for x in atrs])
+
+def getAtrsForUpdate(entidad):
+    """ devuelve los atributos de una entidad para hace un update """
+    atrs = [a for a in entidad['atributos']]
+    at = [''.join( x['nombreatr'] ) for x in atrs]
+    at.remove(getAtrPK(entidad))
+    return ', '.join([x for x in [ (element + ' = ? ') for element in at]])
+
+
